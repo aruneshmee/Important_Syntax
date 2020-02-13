bfa32e4eb8af5a2b1d6167024eb239fdca3424dd
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-02-13 17:46:09 +0800

    Create Alg_basics.py

---
+++ Alg_basics.py
@@ -0,0 +1,20 @@
+import re
+def timeConversion(s):
+    x = re.findall(r"[^\W\d_]+|\d+", s)
+    dn = x[-1]
+    h = x[0]
+    m = x[1]
+    s = x[2]
+    if dn =='AM':
+        if h == '12':
+            print('00'+':'+m+':'+s)
+        else:
+            print(h+':'+m+':'+s)
+    else:
+        if h=='12':
+            print('12'+':'+m+':'+s)
+        else:
+            h = int(h)+12
+            h = str(h)
+            print(h+':'+m+':'+s)
+timeConversion('12:45:54PM')
