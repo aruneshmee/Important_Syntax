2cebab7508231f127c47ecb9ee7e37896b74c749
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-02-21 18:02:51 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -11,3 +11,14 @@ sns.jointplot(x='total_bill',y='tip',data=tips, kind='reg')
 # Graphing evry variable wrt to every other variable
 sns.pairplot(tips)
 sns.pairplot(tips, hue='category') # If categorical data like male or female, 1 or 0, hue can be used to separate with colors
+
+import numpy as np
+sns.barplot(x='sex', y='total_bill', data=tips, estimator=np.std)
+
+sns.barplot(x='sex', y='total_bill', data=tips)
+
+sns.boxplot(x='day',y='total_bill', data=tips, hue='sex')
+
+sns.violinplot(x='day',y='total_bill', data=tips)
+
+sns.stripplot(x='day', y='total_bill', data=tips)
