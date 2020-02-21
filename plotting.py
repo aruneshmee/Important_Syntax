4a27a1b75c1593cd74a4493923877dee2535b21e
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-02-21 16:51:16 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,6 +1,9 @@
+# Seaborn lib proved more options than matplotlib
 import seaborn as sns
 tips = sns.load_dataset('tips')
 
+# Plotting graph with bars
 sns.distplot(tips['total_bill'], kde=False, bins=30)
 
+#Plotting graph with different featues like adding regression line etc
 sns.jointplot(x='total_bill',y='tip',data=tips, kind='reg')
