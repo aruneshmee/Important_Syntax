1cd546a021174cb284e51e87d54061ce3da3c041
Arunesh Mishra <41530378+aruneshmee@users.noreply.github.com>
2020-03-04 16:25:06 +0800

    Update dash_basics.py

--- dash_basics.py
+++ dash_basics.py
@@ -1,5 +1,39 @@
 # Making basic graph app in Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {'background':'#111111','text':'#7FDBFF'}
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello ol',style ={'textAlign':'center','color':colors['text']}),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    ''',),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 9], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
+    
 # Generating Table
 import dash
 import dash_core_components as dcc
